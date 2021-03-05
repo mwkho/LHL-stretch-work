9cb2a48ccc16fbc734e98e914b151ba7d2023e78
mwkho <martin.h@hotmail.ca>
2021-03-05 09:43:40 +0000

    initial test on pythagorea

---
+++ pythagorean.js
@@ -0,0 +1,10 @@
+const input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+const result = input.map((input.x, input.y) => Math.sqrt(input.x**2 + input.y**2));
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
