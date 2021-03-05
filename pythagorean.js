6da8fc82e2cb638d0d4dba6fe03cac70a93a81e6
mwkho <martin.h@hotmail.ca>
2021-03-05 09:52:29 +0000

    completed pythagorean stretch work, by adding an anon arrow function

--- pythagorean.js
+++ pythagorean.js
@@ -3,7 +3,7 @@ const input = [
   { x: 12, y: 5 },
   { x: 8, y: 15 }
 ];
-const result = input.map((input.x, input.y) => Math.sqrt(input.x**2 + input.y**2));
+const result = input.map((element) => Math.sqrt(element.y **2 + element.x**2) );
 
 console.log(result[0] === 5);
 console.log(result[1] === 13);
