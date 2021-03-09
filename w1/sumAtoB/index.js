cc285321b060d02cfc34728a79f27ccca79e59f6
mwkho <martin.h@hotmail.ca>
2021-03-09 05:55:33 +0000

    linted index.js

--- w1/sumAtoB/index.js
+++ w1/sumAtoB/index.js
@@ -1,12 +1,11 @@
 
-function sum(fromN, toN) {
+const sum = function(fromN, toN) {
   //base case when its fromN
-  if (toN === fromN){
+  if (toN === fromN) {
     return toN;
   }
-
   // recursive step of adding
   return toN + sum(fromN, toN - 1);
-}
+};
 
 module.exports = sum;
