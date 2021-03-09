6c0d93ff4c09eeb5c8f99c589b1050a6900e400f
mwkho <martin.h@hotmail.ca>
2021-03-09 05:40:12 +0000

    completed the sales stretch work in w1

--- w1/sales.js
+++ w1/sales.js
@@ -1,48 +1,7 @@
 const salesTax = {
   'AB': 0.05,
   'BC':  0.12
-}
-
-const calculateSalesTax = function(salesData, taxRates){
-  let companyTax = {};
-  for (let company of salesData){
-    // get the information of each company
-    let nameOfCompany = company.name;
-    let province = company.province;
-    let sales = company.sales;
-
-    // first encouter with company, initilize with total sales and tax to 0;
-    if (companyTax[nameOfCompany]){
-      companyTax[nameOfCompany] = {
-        totalSales: 0,
-        totalTax: 0
-      };
-    
-    // add up the sales then apply the provincial tax if its in the tax rates
-    let total = sumArray(sales);
-    companyTax[nameOfCompany].totalSales += total;
-
-
-    if(taxRates[province]){
-      companyTax[nameOfCompany].totalTax += calculateTax(total, taxRates[province])
-    }
-  }
-}
-  return companyTax;
-}
-
-const sumArray = function(arr1){
-  let total = 0;
-  arr1.forEach(element => {total += element
-  });
-  return total;
-}
-
-// helper function that takes in number of sales and returns the total of the tax
-const calculateTax = function(sales, tax){
-  return sales * tax;
-}
-
+};
 
 const companySalesData = [
   {
@@ -62,7 +21,51 @@ const companySalesData = [
   }
 ];
 
-/*
-const results = 
+const sumArray = function(arr) {
+  let total = 0;
+  arr.forEach(element => {
+    total += element;
+  });
+  return total;
+};
 
-*/
+// helper function that takes in number of sales and returns the total of the tax
+const calculateTax = function(sales, tax) {
+  return sales * tax;
+};
+
+
+
+const calculateSalesTax = function(salesData, taxRates) {
+  let companyTax = {};
+  for (let company of salesData) {
+    // get the information of each company
+    let nameOfCompany = company.name;
+    let province = company.province;
+    let sales = company.sales;
+
+    // first encouter with company, initilize with total sales and tax to 0;
+    if (!companyTax[nameOfCompany]) {
+      companyTax[nameOfCompany] = {
+        totalSales: 0,
+        totalTax: 0
+      };
+    }
+
+    // add up the sales then apply the provincial tax if its in the tax rates
+    let total = sumArray(sales);
+    companyTax[nameOfCompany].totalSales += total;
+
+
+    if (taxRates[province]) {
+      companyTax[nameOfCompany].totalTax += calculateTax(total, taxRates[province]);
+    }
+  }
+  return companyTax;
+};
+
+
+
+
+const results = calculateSalesTax(companySalesData, salesTax);
+console.log(results);
