695081fe9eb36f066303299dd0050fe82f32b214
mwkho <martin.h@hotmail.ca>
2021-03-09 05:52:43 +0000

    completed sum A to B recursively

---
+++ w1/sumAtoB/index.js
@@ -0,0 +1,12 @@
+
+function sum(fromN, toN) {
+  //base case when its fromN
+  if (toN === fromN){
+    return toN;
+  }
+
+  // recursive step of adding
+  return toN + sum(fromN, toN - 1);
+}
+
+module.exports = sum;
